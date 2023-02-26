3bb3435e1f2f1de0673a8bc57aa1d3e469fd4285
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2023-02-26 18:01:00 -0500

    Add files via upload

--- tools/js/updatedate.js
+++ tools/js/updatedate.js
@@ -13,10 +13,11 @@ console.log(copyyear);
 //-- Data extraction from picture file to build: "Updated on date" - currency.html
 
 var reqcur = new XMLHttpRequest();
-reqcur.open('GET', 'https://ndurand449.github.io/tools/img/analyze1py.PNG', false);
+reqcur.open('GET', 'https://ndurand449.github.io/tools/img/analyze1pyBW.PNG', false);
 reqcur.send(null);
 gmttextcur = reqcur.getResponseHeader('last-modified');
 updatecur = Intl.DateTimeFormat('en-US', {timeZone: "America/New_York"}).format(Date.parse(gmttextcur))
+console.log(updatecur);
 
 
 //-- Data extraction from picture file to build: "Updated on date" - treasuries.html
@@ -26,3 +27,4 @@ reqtre.open('GET', 'https://ndurand449.github.io/tools/img/DailyAll.PNG', false)
 reqtre.send(null);
 gmttexttre = reqcur.getResponseHeader('last-modified');
 updatetre = Intl.DateTimeFormat('en-US', {timeZone: "America/New_York"}).format(Date.parse(gmttexttre))
+console.log(updatetre);
