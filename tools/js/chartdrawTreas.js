4ea5294f1387ac00650974cfc8760d97f8170b49
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2023-03-12 21:34:39 -0400

    Add files via upload

--- tools/js/chartdrawTreas.js
+++ tools/js/chartdrawTreas.js
@@ -28,9 +28,13 @@ function drawChart0() {
     var chart = new google.visualization.LineChart(document.getElementById('chart_div_Treas0'));
     chart.draw(data, options);
 
-//-- Data extraction from dataTreasTransposedRolling4Fri.js to build: "Updated on date" - treasuries.html
+//-- Data extraction from dataTreasTransposedRolling4Fri.js and dataTreasRolling28.js
+//-- to build: "Updated on date" - treasuries.html
 txt0 = dataTreasTransposedRolling4Fri[0][4];
-document.getElementById("updatetre").innerHTML = "Updated on " + txt0.slice(1,11);
+len = dataTreasRolling28.length;
+d = dataTreasRolling28[len-1][0]
+y = d.getFullYear();
+document.getElementById("updatetre").innerHTML = "Updated on " + txt0 + "/" + y;
 }
 
 
@@ -47,7 +51,7 @@ function drawChart1() {
                 gridlines: {color: '#c1df1f'}},
         hAxis: {title:'Date', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
                 gridlines: {color: '#c1df1f', count: 5}, format: 'MMM d'},
-        legend: {position:'bottom', textStyle: {fontSize: '10'}, maxLines: '5'},
+        legend: {position:'bottom', textStyle: {fontSize: '10'}},
         series: {0:{lineWidth: 1}, 1:{lineWidth: 1}, 2:{lineWidth: 1}, 3:{lineWidth: 2}, 4:{lineWidth: 2}, 
                  5:{lineWidth: 2}, 6:{lineWidth: 2}, 7:{lineWidth: 2}, 8:{lineWidth: 2}, 9:{lineWidth: 1},
                 10:{lineWidth: 1}, 11:{lineWidth: 1, lineDashStyle: [1, 1]}, 12:{lineWidth: 1, lineDashStyle: [1, 1]}},
