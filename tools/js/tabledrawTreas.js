f3a8819f8a21d6c09a49a08869ba5f83f34957b5
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2023-03-17 08:01:11 -0400

    Add files via upload

--- tools/js/tabledrawTreas.js
+++ tools/js/tabledrawTreas.js
@@ -1,7 +1,7 @@
 //----------------------------
-//-- My Tools - v 4.0       --
+//-- My Tools - v 6.0       --
 //-- tabledrawTreas.js      --
-//-- last update 2023-03-05 --
+//-- last update 2023-03-15 --
 //----------------------------
 
 //-- Currency forecast - Google Charts - https://developers.google.com/chart/interactive/docs/
@@ -10,6 +10,14 @@
 function drawTable() {
     var data = google.visualization.arrayToDataTable(dataTreasTransposedRolling5);
 
+    var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
+    formatter.format(data, 1); 
+    formatter.format(data, 2); 
+    formatter.format(data, 3); 
+    formatter.format(data, 4); 
+    formatter.format(data, 5); 
+    formatter.format(data, 6);   
+    
     var table = new google.visualization.Table(document.getElementById('table_div_Treas'));
     table.draw(data, {showRowNumber: false, width: '100%', height: '100%'});
 }
