37813ccb3b6b814c97218d1d4d6b21d0c149f96a
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2023-05-06 19:37:10 -0400

    Add files via upload

--- tools/js/FOMCdata.js
+++ tools/js/FOMCdata.js
@@ -1,13 +1,14 @@
 FOMCcal = [ ['2023', "2023 (cont'd)"],
-	['May 2-3', 'Sept 19-20'],
 	['June 13-14', 'Oct 31 - Nov 1'],
 	['July 25-26', 'Dec 12-13'],
+	['Sept 19-20', null],
 	];
 
 FOMCev = [ ['Date', 'Event'],
 	['02/01', 'FOMC raised the Federal funds rate by 25 bps to 4.75%.'],
 	['03/08', 'Silicon Valley Bank (SVB) announced major losses.'],
 	['03/22', 'FOMC raised the Federal funds rate by 25 bps to 5.00%.'],
+	['05/03', 'FOMC raised the Federal funds rate by 25 bps to 5.25%.'],
 	];
 
 FOMCfr = [ ['Date', 'Rate'],
@@ -21,5 +22,6 @@ FOMCfr = [ ['Date', 'Rate'],
 	[new Date(2022,11,14),4.00],
 	[new Date(2023,1,1),4.50],
 	[new Date(2023,2,22),4.75],
-	[new Date(),5.00],
+	[new Date(2023,4,3),5.00],
+	[new Date(2023,5,13),5.25],
 	];
