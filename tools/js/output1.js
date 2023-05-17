93e32d1b592e9abe4f29120ee746ca108dc1066e
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2023-05-16 22:38:27 -0400

    Add files via upload

--- tools/js/output1.js
+++ tools/js/output1.js
@@ -1,11 +1,11 @@
 var output1 = 
 {
-	lastrdate: "2023-04-06",
+	lastrdate: "2023-05-16",
 	inpry: 2023,
 	Pair: "EUR-USD",
-	avgrate: 1.07456,
+	avgrate: 1.08216,
 	minrate: 1.05219,
 	mindate: "2023-01-05",
-	maxrate: 1.10125,
-	maxdate: "2023-02-01"
+	maxrate: 1.12103,
+	maxdate: "2023-05-05"
 };
--- tools/js/output2.js
+++ tools/js/output2.js
@@ -1,10 +1,10 @@
 var output2 = 
 {
-	lastrdate: "2023-04-06",
+	lastrdate: "2023-05-16",
 	lastrdate2: "2023-12-31",
 	inpry: 2023,
 	Pair: "EUR-USD",
-	avgrate: 1.08787,
-	avgratemin: 1.03592,
-	avgratemax: 1.12571
+	avgrate: 1.09557,
+	avgratemin: 1.05468,
+	avgratemax: 1.13082
 };
