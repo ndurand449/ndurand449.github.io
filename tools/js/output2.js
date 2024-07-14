2fd3654ff5c539fbdb8dbdb2dbff5b03b90b4aef
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2024-07-14 14:50:24 -0400

    Add files via upload

--- tools/js/output2.js
+++ tools/js/output2.js
@@ -1,10 +1,10 @@
 var output2 = 
 {
-	lastrdate: "2024-06-07",
+	lastrdate: "2024-07-14",
 	lastrdate2: "2024-12-31",
 	inpry: 2024,
 	Pair: "EUR-USD",
-	avgrate: 1.08009,
-	avgratemin: 1.05665,
-	avgratemax: 1.10736
+	avgrate: 1.07925,
+	avgratemin: 1.06322,
+	avgratemax: 1.10571
 };
