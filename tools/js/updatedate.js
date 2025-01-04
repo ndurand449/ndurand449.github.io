2b9d9ce44812173dea65c9bfdc656acfed992781
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2025-01-04 09:24:01 -0500

    Add files via upload

--- tools/js/updatedate.js
+++ tools/js/updatedate.js
@@ -1,11 +1,11 @@
 //----------------------------
-//-- My Tools - v 7.0       --
+//-- My Tools - v 9.0       --
 //-- updatedate.js          --
-//-- last update 2023-03-30 --
+//-- last update 2025-01-04 --
 //----------------------------
 
 //-- Current year for footer for html files
 
 var d = new Date();
 copyyear = d.getFullYear();
-document.getElementById("copy").innerHTML = "&copy; " + copyyear + " - ND - version 7.0";
+document.getElementById("copy").innerHTML = "&copy; " + copyyear + " - ND - version 9.0";
