fb95ae511c8646f7db6851d064535fdbf9fc2869
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2025-01-30 08:51:56 -0500

    Add files via upload

--- tools/js/FOMCdata.js
+++ tools/js/FOMCdata.js
@@ -1,15 +1,15 @@
-FOMCcal = [ ["2025", "2025"],
-	['Jan 28-29', 'Jul 29-30'],
-	['Mar 18-19', 'Sep 16-17'],
-	['May 6-7', 'Oct 28-29'],
-	['Jun 17-18', 'Dec 9-10'],
+FOMCcal = [ ["2025", "2025", "2025"],
+	['Mar 18-19', 'Jul 29-30', 'Dec 9-10'],
+	['May 6-7', 'Sep 16-17', null],
+	['Jun 17-18', 'Oct 28-29', null],
 	];
 
 FOMCev = [ ['Date', 'Event'],
-	['07/26/2023', 'FOMC raised the Federal funds rate by 25 bps to a range of 5.25% to 5.50%.'],
+	['07/26/2023', 'FOMC raised the Federal funds rate by 25 bps to a range of 5.25% to 5.50%. (last raise)'],
 	['09/18/2024', 'FOMC lowered the Federal funds rate by 50 bps to a range of 4.75% to 5.00%.'],
 	['11/07/2024', 'FOMC lowered the Federal funds rate by 25 bps to a range of 4.50% to 4.75%.'],
 	['12/18/2024', 'FOMC lowered the Federal funds rate by 25 bps to a range of 4.25% to 4.50%.'],
+	['01/29/2025', 'FOMC maintained the Federal funds rate at a range of 4.25% to 4.50%.'],
 	];
 
 FOMCfr = [ ['Date', 'Rate'],
@@ -25,9 +25,8 @@ FOMCfr = [ ['Date', 'Rate'],
 	[new Date(2023,2,22),4.75],
 	[new Date(2023,4,3),5.00],
 	[new Date(2023,6,25),5.25],
-	[new Date(2023,10,1),5.50],
 	[new Date(2024,8,18),5.50],
 	[new Date(2024,10,7),5.00],
 	[new Date(2024,11,18),4.75],
-	[new Date(2025,0,29),4.50],
+	[new Date(2025,2,19),4.50],
 	];
