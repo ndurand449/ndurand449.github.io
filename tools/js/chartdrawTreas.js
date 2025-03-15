16f284974673ec0806a08e50e9876b7247382d50
ndurand449 <110724429+ndurand449@users.noreply.github.com>
2025-03-15 11:47:35 -0400

    Add files via upload

--- tools/js/chartdrawTreas.js
+++ tools/js/chartdrawTreas.js
@@ -1,7 +1,7 @@
 //----------------------------
-//-- My Tools - v 8.0       --
+//-- My Tools - v 10.4      --
 //-- chartdrawTreas.js      --
-//-- last update 2023-04-03 --
+//-- last update 2025-03-15 --
 //----------------------------
 
 //-- Currency forecast - Google Charts - https://developers.google.com/chart/interactive/docs/
@@ -57,7 +57,7 @@ function drawChart3() {
         vAxis: {title:'Rate (%)', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
                 gridlines: {color: '#c1df1f'}},
         hAxis: {title:'Date', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
-                gridlines: {color: '#c1df1f', count: 5}, format: 'MMM d'},
+                gridlines: {color: '#c1df1f', count: 5}, format: 'MMM yy'},
         tooltip: {trigger:'none'},
     };
 
@@ -83,9 +83,10 @@ function drawChart1() {
         hAxis: {title:'Date', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
                 gridlines: {color: '#c1df1f', count: 5}, format: 'MMM d'},
         legend: {position:'none', textStyle: {fontSize: '11'}},
-        series: {0:{lineWidth: 1}, 1:{lineWidth: 1}, 2:{lineWidth: 1}, 3:{lineWidth: 2}, 4:{lineWidth: 2}, 
-                 5:{lineWidth: 2}, 6:{lineWidth: 2}, 7:{lineWidth: 2}, 8:{lineWidth: 2}, 9:{lineWidth: 1},
-                10:{lineWidth: 1}, 11:{lineWidth: 1, lineDashStyle: [1, 1]}, 12:{lineWidth: 1, lineDashStyle: [1, 1]}},
+        series: {0:{lineWidth: 1}, 1:{lineWidth: 1}, 2:{lineWidth: 1}, 3:{lineWidth: 1}, 4:{lineWidth: 2}, 
+                 5:{lineWidth: 2}, 6:{lineWidth: 2}, 7:{lineWidth: 2}, 8:{lineWidth: 2}, 9:{lineWidth: 2},
+                10:{lineWidth: 1}, 11:{lineWidth: 1}, 12:{lineWidth: 1, lineDashStyle: [1, 1]}, 
+                13:{lineWidth: 1, lineDashStyle: [1, 1]}},
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('chart_div_Treas1'));
@@ -112,37 +113,38 @@ function show(element) {
 }
 
 function myPick() {
-    lW0 = 0; lW1 = 0; lW2 = 0; lW3 = 0; lW4 = 0; lW5 = 0; lW6 = 0; lW7 = 0; lW8 = 0; lW9 = 0; lW10 = 0; lW11 = 0; lW12 = 0;
+    lW0 = 0; lW1 = 0; lW2 = 0; lW3 = 0; lW4 = 0; lW5 = 0; lW6 = 0; lW7 = 0; lW8 = 0; lW9 = 0; lW10 = 0; lW11 = 0; lW12 = 0; lW13 = 0;
     tF0 = false; tF1 = false; tF2 = false; tF3 = false; tF4 = false; tF5 = false; tF6 = false; tF7 = false; tF8 = false; 
-    tF9 = false; tF10 = false; tF11 = false; tF12 = false;
+    tF9 = false; tF10 = false; tF11 = false; tF12 = false; tF13 = false;
 
     if(document.getElementById("treasAll").value == "on") {
-        lW0 = 1; lW1 = 1; lW2 = 1; lW3 = 2; lW4 = 2; lW5 = 2; lW6 = 2; lW7 = 2; lW8 = 2; lW9 = 1; lW10 = 1; lW11 = 1;
-        lW12 = 1; tF0 = true; tF1 = true; tF2 = true; tF3 = true; tF4 = true; tF5 = true; tF6 = true; tF7 = true; 
-        tF8 = true; tF9 = true; tF10 = true; tF11 = true; tF12 = true;
+        lW0 = 1; lW1 = 1; lW2 = 1; lW3 = 1; lW4 = 2; lW5 = 2; lW6 = 2; lW7 = 2; lW8 = 2; lW9 = 2; lW10 = 1; lW11 = 1;
+        lW12 = 1; lW13 = 1; tF0 = true; tF1 = true; tF2 = true; tF3 = true; tF4 = true; tF5 = true; tF6 = true; tF7 = true; 
+        tF8 = true; tF9 = true; tF10 = true; tF11 = true; tF12 = true; tF13 = true;
     } else if(document.getElementById("treasBills").value == "on") {
-        lW0 = 1; lW1 = 1; lW2 = 1; lW3 = 2; lW4 = 2; lW5 = 2; 
-        tF0 = true; tF1 = true; tF2 = true; tF3 = true; tF4 = true; tF5 = true;
+        lW0 = 1; lW1 = 1; lW2 = 1; lW3 = 2; lW4 = 2; lW5 = 2; lW6 = 2; 
+        tF0 = true; tF1 = true; tF2 = true; tF3 = true; tF4 = true; tF5 = true; tF6 = true;
     } else if(document.getElementById("treasNotes").value == "on") {
-        lW6 = 2; lW7 = 2; lW8 = 2; lW9 = 1; lW10 = 1;
-        tF6 = true; tF7 = true; tF8 = true; tF9 = true; tF10 = true;
+        lW7 = 2; lW8 = 2; lW9 = 2; lW10 = 1; lW11 = 1; 
+        tF7 = true; tF8 = true; tF9 = true; tF10 = true; tF11 = true;
     } else if(document.getElementById("treasBonds").value == "on") {
-        lW11 = 1; lW12 = 1;
-        tF11 = true; tF12 = true;
+        lW12 = 1; lW13 = 1;
+        tF12 = true; tF13 = true;
     } else {
         if(document.getElementById("m1").checked) {lW0 = 1; tF0 = true;}
-        if(document.getElementById("m2").checked) {lW1 = 1; tF1 = true;}
-        if(document.getElementById("m3").checked) {lW2 = 1; tF2 = true;}
-        if(document.getElementById("m4").checked) {lW3 = 2; tF3 = true;}
-        if(document.getElementById("m6").checked) {lW4 = 2; tF4 = true;}
-        if(document.getElementById("y1").checked) {lW5 = 2; tF5 = true;}
-        if(document.getElementById("y2").checked) {lW6 = 2; tF6 = true;}
-        if(document.getElementById("y3").checked) {lW7 = 2; tF7 = true;}
-        if(document.getElementById("y5").checked) {lW8 = 2; tF8 = true;}
-        if(document.getElementById("y7").checked) {lW9 = 1; tF9 = true;}
-        if(document.getElementById("y10").checked) {lW10 = 1; tF10 = true;}
-        if(document.getElementById("y20").checked) {lW11 = 1; tF11 = true;}
-        if(document.getElementById("y30").checked) {lW12 = 1; tF12 = true;}
+        if(document.getElementById("m15").checked) {lW1 = 1; tF1 = true;}
+        if(document.getElementById("m2").checked) {lW2 = 1; tF2 = true;}
+        if(document.getElementById("m3").checked) {lW3 = 1; tF3 = true;}
+        if(document.getElementById("m4").checked) {lW4 = 2; tF4 = true;}
+        if(document.getElementById("m6").checked) {lW5 = 2; tF5 = true;}
+        if(document.getElementById("y1").checked) {lW6 = 2; tF6 = true;}
+        if(document.getElementById("y2").checked) {lW7 = 2; tF7 = true;}
+        if(document.getElementById("y3").checked) {lW8 = 2; tF8 = true;}
+        if(document.getElementById("y5").checked) {lW9 = 2; tF9 = true;}
+        if(document.getElementById("y7").checked) {lW10 = 1; tF10 = true;}
+        if(document.getElementById("y10").checked) {lW11 = 1; tF11 = true;}
+        if(document.getElementById("y20").checked) {lW12 = 1; tF12 = true;}
+        if(document.getElementById("y30").checked) {lW13 = 1; tF13 = true;}
     }
     
     document.getElementById("treasAll").checked;
@@ -163,16 +165,16 @@ function drawChart2() {
             vAxis: {title:'Rate (%)', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
                     gridlines: {color: '#c1df1f'}},
             hAxis: {title:'Date', titleTextStyle: {color:'#7d4600', bold: true, italic: false},
-                    gridlines: {color: '#c1df1f', count: 5}, format: 'MMM d'},
+                    gridlines: {color: '#c1df1f', count: 5}, format: 'MMM d, yy'},
             legend: {position:'bottom', textStyle: {fontSize: '11'}},
             series: {0:{lineWidth: lW0, visibleInLegend: tF0}, 1:{lineWidth: lW1, visibleInLegend: tF1}, 
                      2:{lineWidth: lW2, visibleInLegend: tF2}, 3:{lineWidth: lW3, visibleInLegend: tF3}, 
                      4:{lineWidth: lW4, visibleInLegend: tF4}, 5:{lineWidth: lW5, visibleInLegend: tF5}, 
                      6:{lineWidth: lW6, visibleInLegend: tF6}, 7:{lineWidth: lW7, visibleInLegend: tF7}, 
                      8:{lineWidth: lW8, visibleInLegend: tF8}, 9:{lineWidth: lW9, visibleInLegend: tF9},
-                    10:{lineWidth: lW10, visibleInLegend: tF10}, 
-                    11:{lineWidth: lW11, lineDashStyle: [1, 1], visibleInLegend: tF11}, 
-                    12:{lineWidth: lW12, lineDashStyle: [1, 1], visibleInLegend: tF12}},
+                    10:{lineWidth: lW10, visibleInLegend: tF10}, 11:{lineWidth: lW11, visibleInLegend: tF11}, 
+                    12:{lineWidth: lW12, lineDashStyle: [1, 1], visibleInLegend: tF12}, 
+                    13:{lineWidth: lW13, lineDashStyle: [1, 1], visibleInLegend: tF13}},
         };
         var chart = new google.visualization.LineChart(document.getElementById('chart_div_Treas2'));
         chart.draw(data, options);
--- tools/js/updatedate.js
+++ tools/js/updatedate.js
@@ -1,11 +1,11 @@
 //----------------------------
-//-- My Tools - v 10.0       --
+//-- My Tools - v 10.4      --
 //-- updatedate.js          --
-//-- last update 2025-02-25 --
+//-- last update 2025-03-15 --
 //----------------------------
 
 //-- Current year for footer for html files
 
 var d = new Date();
 copyyear = d.getFullYear();
-document.getElementById("copy").innerHTML = "&copy; " + copyyear + " - ND - version 10.0";
+document.getElementById("copy").innerHTML = "&copy; " + copyyear + " - ND - version 10.4";
